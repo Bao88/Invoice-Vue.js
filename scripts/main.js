4944dfc093f16c6e59d3c566d2e1453ffed74fe2
HBN <ngohoangbao@outlook.com>
2018-04-23 21:12:34 +0200

    customer information form added, needs to find the logic behind calculation of vat and net

---
+++ scripts/main.js
@@ -0,0 +1,68 @@
+new Vue({
+    el: "#invoice-creator",
+    data: () => ({
+        valid: true,
+        customer: {
+            name: '',
+            nameRules: [
+            v => !!v || 'Name is required'
+            ],
+        address1: "",
+        address2: "",
+        address3: "",
+        postalCode: "",
+        postalPlace: "",
+        country: "",
+        landline: "",
+        mobile: "",
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+        ]
+        },
+        invoice: {
+            articleNumber: "",
+        text1: "",
+        text2: "",
+        text3: "",
+        unit: "",
+        quantity: "",
+        unitPrice: "",
+        discountRate: "",
+        netPrice: "",
+        vatRate: "",
+        vatAmount: "",
+        items: [
+            { text: 'Norway' },
+            { text: 'Sweden' },
+            { text: 'Denmark' }
+          ]
+        },
+        invoices: []
+      }),
+    
+      methods: {
+        submit () {
+          if (this.$refs.form.validate()) {
+            // Native form submission is not yet supported
+            alert("Send data!");
+            // axios.post('/api/submit', {
+            //   name: this.name,
+            //   email: this.email,
+            //   select: this.select,
+            //   checkbox: this.checkbox
+            // })
+          }
+        },
+        clear () {
+          this.$refs.form.reset()
+        },
+        addInvoice () {
+            this.invoices.push(Vue.util.extend({}, this.invoice))
+        },
+        removeInvoice(index) {
+            Vue.delete(this.invoices, index);
+        }
+      }
+    })
